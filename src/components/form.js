ba514cb60b6504e429513cba4961e0bf96570dbb
Öktem Kalkan <oektem-kalkan@t-online.de>
2023-06-09 11:31:07 +0200

    first commit

---
+++ src/components/form.js
@@ -0,0 +1,14 @@
+export default function Form(){
+    return(
+        <>
+        <h2>Add new Activity:</h2>
+        <h3>Name:</h3>
+        <input type="text"></input>
+        <br/>
+        <h3>Good-Weather activity:</h3>
+        <input type="checkbox"></input>
+        <br/>
+        <button>Submit</button>
+        </>
+    )
+}
